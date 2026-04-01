d49552fcb6ca0c1e4c8e0af505f966831c5003ad
Sean Lally <seanlally8@gmail.com>
2026-04-01 09:13:23 -0400

    fixed line duplication issue

--- src/hackAssembler.c
+++ src/hackAssembler.c
@@ -69,7 +69,6 @@ int main(int argc, char *argv[])
 
       else if (parse == 2){
         strcpy(binary_store[row], "111");
-        strcpy(symbol_store[row], instruction);
         comp(temp, instruction);
         char *avalue = findM(temp);
         compBinary(tempcode, temp);
@@ -92,97 +91,17 @@ int main(int argc, char *argv[])
         memset(tempcode, 0, 17);
         row++;
       }
+      memset(instruction, 0, 200);
     }
 
     else if (hasMoreLines(ptr) == 1){
       // close file
-      for (int g = 0; g < row; g++){
-        printf("%s\n", symbol_store[g]);
-      }
       fclose(ptr);
       break;
     }
   }
 
   // POPULATE SYMBOL TABLE
-  node *hash_table[100] = {0};
-  int new_var = 16;
-  unsigned char init_sym[23][7] = {"R0", "R1", "R2", "R3", "R4", "R5", "R6", "R7", "R8", 
-                                    "R9", "R10", "R11", "R12", "R13", "R14", "R15","SP", 
-                                    "LCL", "ARG", "THIS", "THAT", "SCREEN", "KBD"};
-  unsigned char init_num[23][7] = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", 
-                          "11", "12", "13", "14", "15", "0", "1", "2", "3", "4", 
-                          "16384", "24567"};
-  int hashnum;
-  int line_cntr = -1;
-  char buffer[12] = {0};
-  
-  for (int i = 0; i < 23; i++) {
-    hashnum = hash(init_sym[i]) % 100;
-    hash_table[hashnum] = NULL;
-    node *n = malloc(sizeof(node));
-    strcpy(n->number, (const char *)init_num[i]);
-    strcpy(n->symbol, (const char *)init_sym[i]);
-    hash_table[hashnum] = n;
-  }
-
-  // FIRST PASS TO FIND L INSTRUCTIONS
-  for (int k = 0; k < row; k++){
-    int type = instructionType(symbol_store[k]);
-    symbol(type, temp, symbol_store[k]);
-    hashnum = hash((unsigned char *)temp) % 100;
-    sprintf(buffer, "%i", line_cntr + 1); 
-    if (type == 1 && hash_table[hashnum] == NULL){
-      node *n = malloc(sizeof(node));
-      strcpy(n->number, buffer); 
-      strcpy(n->symbol, temp);
-      n->next = NULL;
-      hash_table[hashnum] = n;
-      memset(buffer, 0, 12);
-      memset(temp, 0, 200);
-    }
-    else if(type == 1 && hash_table[hashnum] != NULL){
-      node *n  = malloc(sizeof(node));
-      strcpy(n->number, buffer);
-      strcpy(n->symbol, temp);
-      n->next = hash_table[hashnum];
-      hash_table[hashnum] = n;
-    }
-    else if (type == 0 || type == 2) {
-      line_cntr++;
-    }
-  }
-
-  // SECOND PASS TO FIND A INSTRUCTIONS
-  for (int p = 0; p < row ; p++){
-    int type = instructionType(symbol_store[p]);
-    symbol(type, temp, symbol_store[p]);
-    hashnum = hash((unsigned char *)temp) % 100;
-    if (type == 0 && hash_table[hashnum] != NULL){
-      for (node *ptr = hash_table[hashnum]; ptr != NULL; ptr = ptr->next){
-        if (strcmp(ptr->symbol, temp) == 0) {
-          symbolBinary(tempcode, ptr->number);
-          strcpy(binary_store[p], tempcode);
-        }
-      }
-      memset(tempcode, 0, 17);
-    }
-    else if (type == 0 && hash_table[hashnum] == NULL) {
-      sprintf(buffer, "%i", new_var);
-      node *n = malloc(sizeof(node));
-      strcpy(n->number, buffer);
-      strcpy(n->symbol, temp);
-      n->next = NULL;
-      hash_table[hashnum] = n;
-      symbolBinary(tempcode, hash_table[hashnum]->number);
-      strcpy(binary_store[p], tempcode);
-      new_var++;
-      memset(tempcode, 0, 17);
-      memset(buffer, 0, 12);
-      free(n);
-    }
-    memset(temp, 0, 200);
-  }
 
 
 
@@ -200,6 +119,10 @@ int main(int argc, char *argv[])
     }
   }
   FILE *hackptr = fopen(newfilename, "w");
+  for (int h = 0; h < row; h++){
+    printf("%s ", binary_store[h]);
+    printf("%i\n", h);
+  }
   for (int z = 0; z < row; z++){ 
     fputs(binary_store[z], hackptr);
     fputc('\n', hackptr);
